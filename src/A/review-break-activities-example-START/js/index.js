9af781a3e5278fca238743de257cb2191fa54eb7
Dan Gilleland <dgilleland@nait.ca>
2025-09-10 11:02:53 -0600

    It works!

--- src/A/review-break-activities-example-START/js/index.js
+++ src/A/review-break-activities-example-START/js/index.js
@@ -6,3 +6,23 @@ HTML for list topic list item
     NEW TOPIC HERE
 </li>
 */
+
+const handleFormSubmit = function(evt) {
+    evt.preventDefault();
+    console.log('Form submitted');
+    const myForm = evt.target;
+    const controls = myForm.elements;
+    console.log(controls);
+    const inputControl = controls['new-topic'];
+    // I can validate, etc.
+    if(inputControl.value.trim() !== '') {
+        // Good input
+        // Add a list item to the unordered list
+        const myList = document.querySelector('ul');
+        myList.innerHTML += `<li>${inputControl.value.trim()}</li>`;
+    }
+}
+
+// Get the only form on the page...
+document.querySelector('form')
+        .addEventListener('submit', handleFormSubmit);
