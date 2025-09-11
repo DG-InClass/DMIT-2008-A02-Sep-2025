6c0ac49803a71414546358b275b524dec4f2c1bb
Dan Gilleland <dgilleland@nait.ca>
2025-09-11 10:38:44 -0600

    Fetching astronaut data

--- src/A/review-astronauts-example-START/js/api/astronaut.js
+++ src/A/review-astronauts-example-START/js/api/astronaut.js
@@ -1,3 +1,10 @@
 const BASE_URL = "https://lldev.thespacedevs.com/2.2.0"
 
 // api functions here.
+const getAstronautList = function() {
+    // Using the Fetch API
+    return fetch(BASE_URL + '/astronaut/')
+            .then(resp => resp.json());
+}
+
+export { getAstronautList }
--- src/A/review-astronauts-example-START/js/index.js
+++ src/A/review-astronauts-example-START/js/index.js
@@ -1,1 +1,7 @@
 // js here.
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { getAstronautList } from './api/astronaut';
+
+getAstronautList().then(data => {
+    console.log(data);
+});
