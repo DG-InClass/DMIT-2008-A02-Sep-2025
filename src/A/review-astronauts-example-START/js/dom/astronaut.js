a1fb2dbe5e87a0bd89f3bf2a8556683fadae1b4b
Dan Gilleland <dgilleland@nait.ca>
2025-09-11 11:30:27 -0600

    Working demo!

--- src/A/review-astronauts-example-START/js/dom/astronaut.js
+++ src/A/review-astronauts-example-START/js/dom/astronaut.js
@@ -16,11 +16,20 @@ replace the instances that have "THIS FORMAT HERE" with the astronautData.
 const renderAstronautListItem = function(astronautData, listElement) {
     // let's interpret astronautData as being the info for a
     // single astronaut.
-    console.log('Astronaut: ', astronautData);
+
     // 1) Extract the data from the object that we want to show
+    const { name, bio, profile_image_thumbnail, nationality, date_of_birth, agency, status } = astronautData;
     // 2) Show the data (render)
-    const newListItem = `<li>Data for ${astronautData.name}</li>`;
-    listElement.innerHTML =+ newListItem;
+    const newListItem = `<li href="#" class="list-group-item list-group-item-action">
+    <div class="d-flex w-100 justify-content-between">
+        <img src="${profile_image_thumbnail}" class="rounded float-start" alt="${name} - ${status.name}">    
+        <h5 class="mb-1">${name} (${status.name})</h5>
+        <small class="float-end">born ${date_of_birth}</small>
+    </div>
+    <small>${nationality} (${agency.name})</small>    
+    <p class="mb-1">${bio} </p>
+</li>`;
+    listElement.innerHTML += newListItem;
 }
 
 export { renderAstronautListItem }
