e6b2e12f74a8fcef73e60d7e427d62f5174aca76
Dan Gilleland <dgilleland@nait.ca>
2025-10-01 11:08:25 -0600

    Demo SASS in React/MUI

--- src/C/nextjs-sass-modules-START/sass-lists-example/components/SimpsonsCharacters.js
+++ src/C/nextjs-sass-modules-START/sass-lists-example/components/SimpsonsCharacters.js
@@ -1,3 +1,5 @@
+import styles from '../styles/SimpsonsCharacters.module.scss';
+
 const SIMPSON_CHARACTERS = [
   "Homer Simpson",
   "Bart Simpson",
@@ -9,11 +11,18 @@ const SIMPSON_CHARACTERS = [
   "Milhouse Van Houten",
   "Ned Flanders",
 ]
+const myListItemStyle = `${styles['list-item']} ${styles.spacing}`;
 
 export default function SimpsonsCharacters() {
-  return <ul>
+  // Remember to use className instead of class
+  // when trying to apply CSS class names to some
+  // HTML tag or Component
+  return <ul className={styles.list}>
     {SIMPSON_CHARACTERS.map((characterName, index)=> {
-      return <li key={index}>{characterName}</li>
+      return <li 
+          key={index}
+          className={myListItemStyle}
+        >{characterName}</li>
     })}
   </ul>
 }
--- src/C/nextjs-sass-modules-START/sass-lists-example/next.config.js
+++ src/C/nextjs-sass-modules-START/sass-lists-example/next.config.js
@@ -1,6 +1,14 @@
+// This line follows the CommonJS (cjs) pattern of importing modules
+const path = require('path'); // I "think" this is the Node:path
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')]
+    //            [ 'C:\\GH\\DMIT2008\...\C\...\styles' ]
+  }
 }
 
 module.exports = nextConfig
