7febd726e9428a800d1860fb92dab295d2fab533
Dan Gilleland <dgilleland@nait.ca>
2025-10-08 11:47:49 -0600

    First demo of state

--- src/D/nextjs-event-state-fundamentals-START/state-events-fundamentals/components/TodoList.js
+++ src/D/nextjs-event-state-fundamentals-START/state-events-fundamentals/components/TodoList.js
@@ -1,5 +1,5 @@
 // ~/components/TodoList.js
-import {Box, Button, Grid, TextField } from '@mui/material';
+import {Box, Button, Grid, TextField, List, ListItem, ListItemText, Typography } from '@mui/material';
 import { useState } from 'react'; // This is the "hook" through which we let React
                                   // know about the data we are managing
 
@@ -9,6 +9,7 @@ export default function TodoList(props) {
     //      |           |                     | The initial value for the data
     //      |           | is used for changing the state
     //      | is used to obtain the state
+    const [allTodos, setAllTodos] = useState([]);
 
     const onTodoTextChange = (event) => {
         console.log(event.target.value);
@@ -17,6 +18,14 @@ export default function TodoList(props) {
 
     const onAddTodoClick = () => {
         console.log('Clicked');
+        // create a new list that has allTodos along with the new todoText.
+        const newAllTodoList = [...allTodos, todoText];
+        //                         \______/
+        console.log(newAllTodoList);
+        // Update the "state" of my todo-list data
+        setAllTodos(newAllTodoList);
+        // reset the value of the todoText
+        setTodoText("");
     }
 
     return <>
@@ -25,5 +34,18 @@ export default function TodoList(props) {
           variant="standard" sx={{width: '100%'}}
           value={todoText} onChange={onTodoTextChange} />
         <Button variant='contained' onClick={onAddTodoClick}>Add Todo</Button>
+        <Grid item xs={12}>
+            <List sx={{width: '100%'}}>
+                {allTodos.map((todoItem, index) =>
+                  <ListItem key={index}>
+                    <ListItemText>
+                        <Typography variant="p" component="div">
+                            {todoItem}
+                        </Typography>
+                    </ListItemText> 
+                  </ListItem>
+                )}
+            </List>
+        </Grid>
     </>
 }
